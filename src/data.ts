ecf73cfb6f8c0b0cf278254a730425bff2a1d4e3
maome1 <miqm783@gmail.com>
2025-09-03 15:51:58 +0300

    Add custom profile image and update avatar URL

--- src/data.ts
+++ src/data.ts
@@ -2,7 +2,7 @@ import type { AppData } from './types';
 
 export const appData: AppData = {
   "profile": {
-    "avatarUrl": "https://picsum.photos/seed/mamo/200",
+    "avatarUrl": "./profile.png",
     "name": "MAOME",
     "title": "",
     "bio": "سُرَّ منّ رأى || 1410 هـ\n\"الكَلِمّة الطيّبة، تجبّر الخاطر\"\n(لفقيدتي الجَنَّة، وليَّ الصَّبر)"
